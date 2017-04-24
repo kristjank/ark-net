99e5d66e98837b578edb460be616af904947af4d
chris <kristjan.kosic@gmail.com>
2017-04-24 15:46:55 +0200

    initial structure and project type setting up

---
+++ ark-net/io/ark/core/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ark_net.io.ark.core
+{
+    class Account
+    {
+        String address;
+        String publicKey;
+        long balance;
+        String username;
+        int rate;
+        List<String> votes;
+
+        public string Address { get => address; set => address = value; }
+    }
+}
