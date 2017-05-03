3a551968f8c8b9ad3c7b3a22544f291f8cb11413
chris <kristjan.kosic@gmail.com>
2017-05-03 15:28:10 +0200

    starting to implement user friendly usage code

--- ark-net/Controller/AccountController.cs
+++ ark-net/Controller/AccountController.cs
@@ -1,42 +1,86 @@
-﻿using System.Collections.Generic;
-using ArkNet.Core;
+﻿using System;
+using ArkNet.Model;
 
 namespace ArkNet.Controller
 {
-	public class AccountController
-	{
-		public string Address { get; set; }
+    public class AccountController
+    {
+        private Account _account;
 
-		public string PublicKey { get; set; }
+        public AccountController(Account arkaccount)
+        {
+            this._account = arkaccount;
+        }
 
-		public long Balance { get; set; }
+        public bool AskRemoteSignature()
+        {
+            throw new NotImplementedException();
+        }
 
-		public string Username { get; set; }
+        public void SendMultisignArk()
+        {
+            throw new NotImplementedException();
+        }
 
-		public int Rate { get; set; }
+        public bool OpenAccount(string passphrase)
+        {
 
-		public List<string> Votes { get; set; }
 
-		public bool ApplyTransaction(TransactionApi transaction)
-		{
-			Balance -= transaction.Amount + transaction.Fee;
-			return Balance > -1;
-		}
+            throw new NotImplementedException();
+        }
 
-		public bool UndoTransaction(TransactionApi transaction)
-		{
-			Balance += transaction.Amount + transaction.Fee;
-			return Balance > -1;
-		}
+        public bool SendArk()
+        {
+            throw new NotImplementedException();
+        }
 
-		public Verification VerifyTransaction(TransactionApi transaction)
-		{
-			var v = new Verification();
-			/*if (Balance >= transaction.Amount + transaction.Fee)
-				v.AddError(string.Format("Account %1 does not have enough balance: %2", Address, Balance));*/
-			// TODO: many things
+        public bool VoteForDelegate()
+        {
+            throw new NotImplementedException();
+        }
 
-			return v;
-		}
-	}
+        public bool RegisterAsDelegate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoteSign()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RegisterSecondSignature()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetVoterContribution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+/*public static bool ApplyTransaction(Account account, long amount)
+{
+
+
+    /*Balance -= transaction.Amount + transaction.Fee;
+    return Balance > -1;
+}
+
+public static bool UndoTransaction(TransactionApi transaction)
+{
+    /*Balance += transaction.Amount + transaction.Fee;
+    return Balance > -1;
+}
+
+public static Verification VerifyTransaction(TransactionApi transaction)
+{
+    var v = new Verification();
+    if (Balance >= transaction.Amount + transaction.Fee)
+        v.AddError(string.Format("Account %1 does not have enough balance: %2", Address, Balance));
+    // TODO: many things
+
+    return v;
+}*/
 }
---
+++ ark-netTests/Controller/AccountControllerTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArkNet.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArkNet.Model;
+
+namespace ArkNet.Controller.Tests
+{
+    [TestClass()]
+    public class AccountControllerTests
+    {
+        [TestMethod()]
+        public void AccountControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AskRemoteSignatureTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendMultisignArkTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void OpenAccountTest()
+        {
+            var accCtnrl = new AccountController(new Account());
+            accCtnrl.OpenAccount("this is a top secret passphrase");
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendArkTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void VoteForDelegateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RegisterAsDelegateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoteSignTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RegisterSecondSignatureTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetVoterContributionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
