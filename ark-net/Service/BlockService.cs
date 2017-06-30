378e3dcb7524925d31978735492ecde4355968c7
chbt <chbta>
2017-07-01 00:58:52 +0200

    Added BlockService.

---
+++ ark-net/Service/BlockService.cs
@@ -0,0 +1,36 @@
+﻿using ArkNet.Core;
+using ArkNet.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace ArkNet.Service
+{
+    public class BlockService
+    {
+        public static ArkBlock GetById(string id)
+        {
+            var response = NetworkApi.Instance.ActivePeer.MakeRequest("GET", "/api/blocks/get?id=" + id);
+            var parsed = JObject.Parse(response);
+
+            var block = new ArkBlock();
+            if (!Convert.ToBoolean(parsed["success"]))
+                block.Id = parsed["error"].ToString();
+            else
+                block = JsonConvert.DeserializeObject<ArkBlock>(parsed["block"].ToString());
+            return block;
+        }
+
+        public static IEnumerable<ArkBlock> GetAll()
+        {
+            var response = NetworkApi.Instance.ActivePeer.MakeRequest("GET", "/api/blocks");
+
+            var parsed = JObject.Parse(response);
+            var array = (JArray)parsed["blocks"];
+
+            var blockList = JsonConvert.DeserializeObject<IReadOnlyCollection<ArkBlock>>(array.ToString());
+            return blockList;
+        }
+    }
+}
