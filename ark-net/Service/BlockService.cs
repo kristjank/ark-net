fb64e8e569a2db8b7b731315e178718d724e81cc
sharkdev-j <sharkarkdev96@gmail.com>
2017-09-27 17:17:12 -0700

    Closes #17

--- ark-net/Service/BlockService.cs
+++ ark-net/Service/BlockService.cs
@@ -1,5 +1,6 @@
 ﻿using ArkNet.Core;
 using ArkNet.Model;
+using ArkNet.Utils;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
@@ -31,6 +32,68 @@ namespace ArkNet.Service
 
             var blockList = JsonConvert.DeserializeObject<IReadOnlyCollection<ArkBlock>>(array.ToString());
             return blockList;
+        }
+
+        public static DateTime GetEpoch()
+        {
+            var response = NetworkApi.Instance.ActivePeer.MakeRequest("GET", "/api/blocks/getEpoch");
+            var parsed = JObject.Parse(response);
+
+            return DateTime.Parse(parsed["epoch"].ToString());
+        }
+
+        public static long GetHeight()
+        {
+            var response = NetworkApi.Instance.ActivePeer.MakeRequest("GET", "/api/blocks/getHeight");
+            var parsed = JObject.Parse(response);
+
+            return Int64.Parse(parsed["height"].ToString());
+        }
+
+        public static string GetNetHash()
+        {
+            var response = NetworkApi.Instance.ActivePeer.MakeRequest("GET", "/api/blocks/getNethash");
+            var parsed = JObject.Parse(response);
+
+            return parsed["nethash"].ToString();
+        }
+
+        public static Fees GetFees()
+        {
+            var response = NetworkApi.Instance.ActivePeer.MakeRequest("GET", "/api/blocks/getFees");
+            var parsed = JObject.Parse(response);
+
+            return JsonConvert.DeserializeObject<Fees>(parsed["fees"].ToString());
+        }
+
+        public static int GetMilestone()
+        {
+            var response = NetworkApi.Instance.ActivePeer.MakeRequest("GET", "/api/blocks/getMilestone");
+            var parsed = JObject.Parse(response);
+
+            return Int32.Parse(parsed["milestone"].ToString());
+        }
+
+        public static int GetReward()
+        {
+            var response = NetworkApi.Instance.ActivePeer.MakeRequest("GET", "/api/blocks/getReward");
+            var parsed = JObject.Parse(response);
+
+            return Int32.Parse(parsed["reward"].ToString());
+        }
+
+        public static long GetSupply()
+        {
+            var response = NetworkApi.Instance.ActivePeer.MakeRequest("GET", "/api/blocks/getSupply");
+            var parsed = JObject.Parse(response);
+
+            return Int64.Parse(parsed["supply"].ToString());
+        }
+
+        public static ArkBlockChainStatus GetStatus()
+        {
+            var response = NetworkApi.Instance.ActivePeer.MakeRequest("GET", "/api/blocks/getStatus");
+            return JsonConvert.DeserializeObject<ArkBlockChainStatus>(response);
         }
     }
 }
