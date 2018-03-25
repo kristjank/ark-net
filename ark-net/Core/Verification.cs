a27702eaaa4822c23f369ae8b8ff5a758ef92883
Djenny Floro <jmoulard@gmail.com>
2018-03-25 18:01:29 +0200

    Update Verification.cs

--- ark-net/Core/Verification.cs
+++ ark-net/Core/Verification.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Verification.cs" company="Ark Labs">
+// <copyright file="Verification.cs" company="Ark">
 //   MIT License
 //   // 
 //   // Copyright (c) 2017 Kristjan Košič
