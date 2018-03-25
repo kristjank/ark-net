7b381e42f995054b4a081f205afcca2b83368274
Djenny Floro <jmoulard@gmail.com>
2018-03-25 18:04:36 +0200

    Update ArkAccountResponse.cs

--- ark-net/Model/Account/ArkAccountResponse.cs
+++ ark-net/Model/Account/ArkAccountResponse.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ArkAccountResponse.cs" company="Ark Labs">
+// <copyright file="ArkAccountResponse.cs" company="Ark">
 //   MIT License
 //   // 
 //   // Copyright (c) 2017 Kristjan Košič
