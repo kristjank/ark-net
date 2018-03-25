a64bdf0733219f81110806c77d17704c3e44326c
Djenny Floro <jmoulard@gmail.com>
2018-03-25 18:02:30 +0200

    Update ArkBlockRequest.cs

--- ark-net/Messages/Block/ArkBlockRequest.cs
+++ ark-net/Messages/Block/ArkBlockRequest.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ArkBlockRequest.cs" company="Ark Labs">
+// <copyright file="ArkBlockRequest.cs" company="Ark">
 //   MIT License
 //   // 
 //   // Copyright (c) 2017 Kristjan Košič
