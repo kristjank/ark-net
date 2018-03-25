9c493ccb9fc47ede4654b2ed5f5afda7df152590
Djenny Floro <jmoulard@gmail.com>
2018-03-25 18:01:11 +0200

    Update TransactionApi.cs

--- ark-net/Core/TransactionApi.cs
+++ ark-net/Core/TransactionApi.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="TransactionApi.cs" company="Ark Labs">
+// <copyright file="TransactionApi.cs" company="Ark">
 //   MIT License
 //   // 
 //   // Copyright (c) 2017 Kristjan Košič
