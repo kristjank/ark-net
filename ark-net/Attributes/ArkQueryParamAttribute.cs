5a5f2d26528e05e9053e538ebfe16d5c5790a2c9
Djenny Floro <jmoulard@gmail.com>
2018-03-25 17:56:21 +0200

    Update ArkQueryParamAttribute.cs

--- ark-net/Attributes/ArkQueryParamAttribute.cs
+++ ark-net/Attributes/ArkQueryParamAttribute.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ArkQueryParamAttribute.cs" company="Ark Labs">
+// <copyright file="ArkQueryParamAttribute.cs" company="Ark">
 //   MIT License
 //   // 
 //   // Copyright (c) 2017 Kristjan Košič
