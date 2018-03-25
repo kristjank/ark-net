d82ee6eacd6e2ac185456bd8e2d04679158d98f3
Djenny Floro <jmoulard@gmail.com>
2018-03-25 17:57:49 +0200

    Update AccountController.cs

--- ark-net/Controller/AccountController.cs
+++ ark-net/Controller/AccountController.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AccountController.cs" company="Ark Labs">
+// <copyright file="AccountController.cs" company="Ark">
 //   MIT License
 //   // 
 //   // Copyright (c) 2017 Kristjan Košič
