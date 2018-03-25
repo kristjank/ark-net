7df3e480be0d04ad96bbdc698b7d0d23c7f109db
Djenny Floro <jmoulard@gmail.com>
2018-03-25 18:02:08 +0200

    Update ArkBaseRequest.cs

--- ark-net/Messages/BaseMessages/ArkBaseRequest.cs
+++ ark-net/Messages/BaseMessages/ArkBaseRequest.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ArkBaseRequest.cs" company="Ark Labs">
+// <copyright file="ArkBaseRequest.cs" company="Ark">
 //   MIT License
 //   // 
 //   // Copyright (c) 2017 Kristjan Košič
