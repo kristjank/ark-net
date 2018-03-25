6891f4d76b8cfc2f7fd5c64fbdc5759eb1a05e3e
Djenny Floro <jmoulard@gmail.com>
2018-03-25 18:00:52 +0200

    Update Slot.cs

--- ark-net/Core/Slot.cs
+++ ark-net/Core/Slot.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Slot.cs" company="Ark Labs">
+// <copyright file="Slot.cs" company="Ark">
 //   MIT License
 //   // 
 //   // Copyright (c) 2017 Kristjan Košič
