e2a2be2c0753ab00487a00081b2bdd58e0fd6389
Djenny Floro <jmoulard@gmail.com>
2018-03-26 02:16:49 +0200

    Update ArkResponseBase.cs

--- ark-net/Model/BaseModels/ArkResponseBase.cs
+++ ark-net/Model/BaseModels/ArkResponseBase.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ArkResponseBase.cs" company="Ark Labs">
+// <copyright file="ArkResponseBase.cs" company="Ark">
 //   MIT License
 //   // 
 //   // Copyright (c) 2017 Kristjan Košič
