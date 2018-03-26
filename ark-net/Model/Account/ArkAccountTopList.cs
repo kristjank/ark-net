557a566d6ab785d8813a1053fadf11c23b88b33d
Djenny Floro <jmoulard@gmail.com>
2018-03-26 02:16:25 +0200

    Update ArkAccountTopList.cs

--- ark-net/Model/Account/ArkAccountTopList.cs
+++ ark-net/Model/Account/ArkAccountTopList.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ArkAccountTopList.cs" company="Ark Labs">
+// <copyright file="ArkAccountTopList.cs" company="Ark">
 //   MIT License
 //   // 
 //   // Copyright (c) 2017 Kristjan Košič
