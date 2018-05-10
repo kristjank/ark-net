fdffc5b44a46c43127c45bb2d5e69e575030f2a9
sharkdev-j <sharkarkdev96@gmail.com>
2018-05-09 19:15:48 -0700

    Added logging framework

---
+++ ark-net/Core/LoggingApi.cs
@@ -0,0 +1,34 @@
+﻿using ArkNet.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArkNet.Core
+{
+    public class LoggingApi
+    {
+        private IArkLogger _logger;
+        public LoggingApi(IArkLogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Log(ArkLogLevel logLevel, string message)
+        {
+            if (_logger != null)
+                _logger.Log(new ArkLogEntry(logLevel, message));
+        }
+
+        public void Log(ArkLogLevel logLevel, Exception exception)
+        {
+            if (_logger != null)
+                _logger.Log(new ArkLogEntry(logLevel, exception.Message, exception));
+        }
+
+        public void Log(ArkLogLevel logLevel, string message, Exception exception)
+        {
+            if (_logger != null)
+                _logger.Log(new ArkLogEntry(logLevel, message, exception));
+        }
+    }
+}
---
+++ ark-net/Logging/ArkLoggingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArkNet.Logging
+{
+    public static class ArkLoggingExtensions
+    {
+        public static void Log(this IArkLogger logger, string message)
+        {
+            logger.Log(new ArkLogEntry(ArkLogLevel.Info, message));
+        }
+
+        public static void Log(this IArkLogger logger, Exception exception)
+        {
+            logger.Log(new ArkLogEntry(ArkLogLevel.Error, exception.Message, exception));
+        }
+
+        public static void Log(this IArkLogger logger, string message, Exception exception)
+        {
+            logger.Log(new ArkLogEntry(ArkLogLevel.Error, message, exception));
+        }
+    }
+}
